9df843d81b80260af6a1ba360843f62f0531b72d
Epiq122 <l2ob122@gmail.com>
2022-06-13 21:28:17 -0700

    finished main section and styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import data from './data';
 
 import Header from './components/Header/Header';
-// import Main from './components/Main/Main';
+import Main from './components/Main/Main';
 function App() {
+  const cardData = data.map((data) => {
+    return <Main key={data.id} {...data} />;
+  });
   return (
     <div className='App'>
-      <h1>
-        <Header />
-      </h1>
-      <h2>{/* <Main /> */}</h2>
+      <Header />
+
+      {cardData}
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,24 @@
-function Main() {
-  return <div>Main</div>;
-}
+import blip from '../../img/blip.png';
+import './Main.css';
 
+function Main(props) {
+  return (
+    <main>
+      <div className='main--card'>
+        <img className='places--img' src={props.imageUrl} alt='img' />
+        <div className='info--container'>
+          <img className='blip--img' src={blip} alt='' />
+          <span className='country--name'>{props.location}</span>
+          <a href={props.googleMapsUrl}>View on Google Maps</a>
+          <h2>{props.title}</h2>
+          <span className='span--date'>
+            {props.startDate} - {props.endDate}
+          </span>
+          <p>{props.description}</p>
+        </div>
+        <div className='border'></div>
+      </div>
+    </main>
+  );
+}
 export default Main;
